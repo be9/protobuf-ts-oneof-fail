a9477a216a72a45e4471f4142e45a1eb1b6f4e2f
Oleg Dashevskii <olegdashevskii@gmail.com>
2022-08-25 12:30:29 +0700

    add

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { getOneofValue, isOneofGroup } from '@protobuf-ts/runtime';
+
+type SimpleOneof =
+    | { oneofKind: "a"; a: string; }
+    | { oneofKind: "b"; b: number; }
+    | { oneofKind: "c"; c: boolean; }
+    | { oneofKind: undefined; };
+
+// this works (similar to oneof.spec.ts)
+function simpleTest(oneof: SimpleOneof): string | undefined {
+    return getOneofValue(oneof, 'a');
+}
+
+
+interface Foo {
+    foo: string;
+}
+
+interface Bar {
+    bar: string;
+}
+
+type FooBarOneof =
+    | { oneofKind: "foo"; foo: Foo; }
+    | { oneofKind: "bar"; bar: Bar; }
+    | { oneofKind: undefined; };
+
+function fooBarTest(oneof: FooBarOneof): void {
+/*
+Argument of type 'FooBarOneof' is not assignable to parameter of type 'UnknownOneofGroup'.
+  Type '{ oneofKind: "foo"; foo: Foo; }' is not assignable to type 'UnknownOneofGroup'.
+    Property 'foo' is incompatible with index signature.
+      Type 'Foo' is not assignable to type 'UnknownScalar | UnknownMessage | undefined'.
+        Type 'Foo' is not assignable to type 'UnknownMessage'.
+          Index signature for type 'string' is missing in type 'Foo'.ts(2345)    
+*/
+    getOneofValue(oneof, 'foo');
+
+    if (isOneofGroup(oneof)) {
+        // Argument of type 'string' is not assignable to parameter of type 'never'.ts(2345)        
+        getOneofValue(oneof, 'bar');
+    }
+
+}
